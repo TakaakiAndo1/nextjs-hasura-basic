d77291ede94d7981c94836c47d71cb4547e55f8c
Takaaki Ando <z.takaaki.ando@gree.net>
2023-03-01 17:12:57 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout } from '../components/Layout'
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <p className="text-3xl font-bold">Next.js + GraphQL</p>
+    </Layout>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
