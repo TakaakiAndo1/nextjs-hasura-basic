9aaf75243820d2c8fe4eb31ae51d189d4caf98a4
Takaaki Ando <z.takaaki.ando@gree.net>
2023-03-04 01:03:37 +0900

    HasuraのURLや認証情報を環境変数化

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -11,11 +11,10 @@ const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      // uri: process.env.NEXT_PUBLIC_HASURA_URL,
-      uri: 'https://basic-lesson-hasuraz.hasura.app/v1/graphql',
-      // headers: {
-      //   'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
-      // },
+      uri: process.env.NEXT_PUBLIC_HASURA_URL,
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
+      },
     }),
     cache: new InMemoryCache(),
   })
