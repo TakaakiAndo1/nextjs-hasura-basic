6f32c7f0f9fa36b389f748c2197b15e70388cbb2
Takaaki Ando <z.takaaki.ando@gree.net>
2023-03-03 15:02:15 +0900

    apollo clientを使用したページを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import { initializeApollo } from '../lib/apolloClient'
+import { ApolloProvider } from '@apollo/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = initializeApollo()
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
