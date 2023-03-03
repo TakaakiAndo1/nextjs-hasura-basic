17015b01b8554fed48b9ab0aa11cef24f6ecdb29
Takaaki Ando <z.takaaki.ando@gree.net>
2023-03-04 01:00:34 +0900

    コメントコード削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
