d8e6169145277fb9ca64a4fc24b4a1dab6fd0e13
Takaaki Ando <z.takaaki.ando@gree.net>
2023-03-03 23:05:52 +0900

    各ページのtestコード(jest+testing-library+MSW)を追加

---
+++ __tests__/HasuraMain.test.tsx
@@ -0,0 +1,37 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { setupServer } from 'msw/node'
+import { getPage, initTestHelpers } from 'next-page-tester'
+import { handlers } from '../mock/handlers'
+import 'setimmediate'
+
+initTestHelpers()
+
+const server = setupServer(...handlers)
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('Hasura Fetch Test Cases', () => {
+  it('Should render the list of users by useQuery', async () => {
+    const { page } = await getPage({
+      route: '/hasura-main',
+    })
+    render(page)
+    expect(await screen.findByText('Hasura main page')).toBeInTheDocument()
+    // Note: APIデータが返却されるまでタイムラグがあるため、ここでawait指定をしておく
+    expect(await screen.findByText('Test user A')).toBeInTheDocument()
+    expect(screen.getByText('Test user B')).toBeInTheDocument()
+    expect(screen.getByText('Test user C')).toBeInTheDocument()
+  })
+})
